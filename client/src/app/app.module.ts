a0eeddf12550a4eadb726152b671322c92fb0281
Gui <gsardaparreu@gmail.com>
2022-12-07 17:58:32 +0100

    style: format default files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ButtonComponent } from './button/button.component';
+import { CounterComponent } from './counter/counter.component';
+import { BoxCounterComponent } from './box-counter/box-counter.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DashboardComponent, ButtonComponent, CounterComponent, BoxCounterComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
